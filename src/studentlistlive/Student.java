c72ffd29abf98c3342240466fae20052fa6bc885
Juyoung Jung <84524297+ju-young-jung@users.noreply.github.com>
2021-05-26 21:37:30 -0400

    Update Student.java
    added a description

--- src/studentlistlive/Student.java
+++ src/studentlistlive/Student.java
@@ -1,12 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package studentlistlive;
 
 /**
- * This class creates a list of students
+ * This class creates a list of students.
+ *
  * @author Juyoung Jung, May 20 2021
  */
 
