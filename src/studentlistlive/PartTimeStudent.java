056501b7e5c4f3226c9923d1503cea517e98b017
Juyoung Jung <jjuyoung0801@gmail.com>
2021-05-26 21:49:36 -0400

    new branch

---
+++ src/studentlistlive/PartTimeStudent.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package studentlistlive;
+
+/**
+ * This class is for a git demo only
+ * @author Juyoung Jung;
+ */
+public class PartTimeStudent {
+    
+}
